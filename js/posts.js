cdbfd070191be13e51275280b03e15cf52007910
Almudena Vigara <almudena.vigara@preply.com>
2024-06-25 18:07:16 +0200

    Home and post pages

---
+++ js/posts.js
@@ -0,0 +1,54 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const params = new URLSearchParams(window.location.search);
+    const postId = parseInt(params.get('id'));
+
+    fetch('../posts.json')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data, "Data fetched successfully");
+
+            const post = data.find(p => p.id === postId);
+            console.log(post, "Post found");
+
+            if (post) {
+                const postTitleElement = document.getElementById('post-title');
+                const postImageElement = document.getElementById('post-image');
+                const preparationTitleElement = document.getElementById('preparation-title');
+                const preparationExplanationElement = document.getElementById('preparation-explanation');
+                const ingredientsListElement = document.getElementById('post-ingredients');
+
+                console.log(postTitleElement, "Post title element");
+                console.log(postImageElement, "Post image element");
+                console.log(preparationTitleElement, "Preparation title element");
+                console.log(preparationExplanationElement, "Preparation explanation element");
+                console.log(ingredientsListElement, "Ingredients list element");
+
+                postTitleElement.textContent = post.title;
+                postImageElement.src = `../${post.image}`;
+
+                // Convertir saltos de línea en <br> para la explicación de la preparación
+                const preparationExplanation = post.preparation.explanation.replace(/\n/g, '<br>');
+                preparationExplanationElement.innerHTML = preparationExplanation;
+
+                if (post.ingredients) {
+                    post.ingredients.forEach(ingredient => {
+                        const li = document.createElement('li');
+                        li.textContent = ingredient;
+                        ingredientsListElement.appendChild(li);
+                    });
+                } else {
+                    console.warn('No ingredients found for this post');
+                }
+
+                if (post.preparation) {
+                    preparationTitleElement.textContent = post.preparation.title;
+                } else {
+                    console.warn('No preparation details found for this post');
+                }
+            } else {
+                console.error('Post not found');
+                document.getElementById('post-title').textContent = 'Post not found';
+            }
+        })
+        .catch(error => console.error('Error fetching the post:', error));
+});
---
+++ js/script.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('/posts.json')
+        .then(response => response.json())
+        .then(data => {
+            const container = document.getElementById('posts-container');
+            data.forEach(post => {
+                const card = document.createElement('div');
+                card.className = 'card';
+                card.innerHTML = `
+                    <img src="${post.image}" alt="${post.title}">
+                    <div class="card-content">
+                        <h2 class="card-title">${post.title}</h2>
+                        <p class="card-date">${post.date}</p>
+                        <a href="pages/post.html?id=${post.id}" class="card-links"><p>Read more</p></a>
+                       
+                    </div>
+                `;
+                card.addEventListener('click', () => {
+                    window.location.href = `/pages/post.html?id=${post.id}`;
+                });
+                container.appendChild(card);
+                console.log(data,"los datos estan aqui");
+            });
+        })
+        .catch(error => console.error('Error fetching the posts:', error));
+});
